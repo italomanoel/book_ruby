c243f019e94ba85a6a7ef133e6dd36a9a85feec9
italomanoel <manoel.italo89@gmail.com>
2020-05-10 16:07:02 -0300

    terminado exemplo de declarar strings

--- formas_declarar_strings.rb
+++ formas_declarar_strings.rb
@@ -1,4 +1,10 @@
 aspas_simples = 'linguagem Ruby. '
 aspas_duplas = "Linguagem Ruby. "
+
 string_especial_usando_aspas_simples = 'Usando "duplas" e \'simples\' em Ruby. ' 
 string_especial_usando_aspas_duplas = "Usando 'simples' e \"duplas\" em Ruby. "
+
+# Se pode usar qualquer caractere não alfa numérico após % para delimitar, exemplos ()[]{}??~~
+string_especial = %{Isso é "normal" e 'útil' em Ruby. }
+
+puts aspas_simples + aspas_duplas + string_especial + string_especial_usando_aspas_duplas + string_especial_usando_aspas_simples
