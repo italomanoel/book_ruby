3622c2c31d2c36a062c0b4c90a00af63ffffeb90
italomanoel <manoel.italo89@gmail.com>
2020-05-11 22:05:02 -0300

    .

---
+++ livro_projeto/dados.rb
@@ -0,0 +1,17 @@
+class Livro
+end
+=begin
+def preco_com_desconto(preco, desconto)
+    preco - (preco * desconto)
+end
+
+nome = "Nome"
+isbn = "codigo"
+numero_paginas = 212
+
+preco = 89.90
+desconto = 0.1
+
+puts preco_com_desconto(preco, desconto)
+
+=end
