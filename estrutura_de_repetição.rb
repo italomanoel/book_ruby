59d824874a271024e8c557920ef5e1ce202a0d10
italomanoel <manoel.italo89@gmail.com>
2020-05-13 19:12:49 -0300

    terminado exemplo. E

--- estrutura_de_repetição.rb
+++ estrutura_de_repetição.rb
@@ -1,1 +1,17 @@
 #colocar exemplos de estrutura de repetição FOR WHILE UNTIL
+for numero_for in (1..5)
+    puts "Numero com for #{numero_for}" 
+    numero_for += 1
+end
+
+numero_while = 0
+while numero_while <= 10
+    puts "Numero com While #{numero_while}"
+    numero_while += 1
+end
+
+numero_until = 0 
+until numero_until == 5
+    puts "Numero com until #{numero_until}"
+    numero_until += 1
+end
