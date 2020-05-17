d691fa78f3b0b46d42055fd39ebf8219410d9cd1
italomanoel <manoel.italo89@gmail.com>
2020-05-17 03:25:25 -0300

    page 41 55

--- loja_virtual/lib/livro.rb
+++ loja_virtual/lib/livro.rb
@@ -1,14 +1,23 @@
 # enconding: utf-8
 class Livro
+    #o último parâmetro deve estar vazio.
     def initialize(autor, isbn = "1", numero_de_paginas, preco)
         @autor = autor
         @isbn = isbn
         @numero_de_paginas = numero_de_paginas
-        @preco = preco
+        @preco = preco        
         # puts "Autor: #{autor}, ISBN: #{isbn}, Páginas: #{numero_de_paginas}"
     end
 
     def to_s
         "Autor(a): #{@autor}, ISBN: #{@isbn}, Páginas: #{@numero_de_paginas}"
+    end
+
+    def preco
+        @preco
+    end
+
+    def preco=(preco)
+        @preco = preco
     end
 end
--- loja_virtual/resultado_variaveis_de_instancia.rb
+++ loja_virtual/resultado_variaveis_de_instancia.rb
@@ -1,9 +1,9 @@
 require File.expand_path("lib/livro")
 
-teste_e_design = Livro.new("Mauricio Li", "0887", 200)
-web_design_responsive = Livro.new("Kuha Chu", "087070", 1203)
+teste_e_design = Livro.new("Mauricio Li", "0887", 247, 60.9)
+teste_e_design.preco = 79.9
+#web_design_responsive = Livro.new("Kuha Chu", "087070", 1203, 120)
 
-#p teste_e_design
-puts web_design_responsive, teste_e_design
-
+puts teste_e_design.preco
+#puts teste_e_design, web_design_responsive
 
