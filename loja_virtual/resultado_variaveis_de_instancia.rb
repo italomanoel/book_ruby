d37e4b1782a5188264e23213ecf2037e181b4294
italomanoel <manoel.italo89@gmail.com>
2020-05-28 20:16:59 -0300

    cap 4.1

--- loja_virtual/resultado_variaveis_de_instancia.rb
+++ loja_virtual/resultado_variaveis_de_instancia.rb
@@ -7,7 +7,7 @@ puts teste_e_design.preco
 #web_design_responsive = Livro.new("Kuha Chu", "087070", 1203, 120)
 #puts teste_e_design, web_design_responsive
 
-#usando o instance_variable_get e set, burlando método privado, não faça!
+#usando o instance_variable_get e set, burlando método privado, não faça! Apenas exemplo
 teste_e_design = Livro.new("Mauricio Li", "0887", 247, 60.9)
 puts teste_e_design.instance_variable_get "@preco"
 
